453bf59fd051cd10b909893c1f328cae0bf15918
krutika12yeola <89001809+krutika12yeola@users.noreply.github.com>
2023-12-23 01:08:36 +0530

    Add files via upload

---
+++ QuestionBank.js
@@ -0,0 +1,34 @@
+const qBank = [
+    {
+        id: 1,
+        question: "'OS' computer abbreviation usually means ?",
+        options: ["Order of Significance","Open Software", "Operating System"],
+        answer: "Operating System",
+    },
+    {
+        id: 2,
+        question: ".INI extension refers usually to what kind of file?",
+        options: ["Hypertext related file", "System file","Image Color Matching Profile file"],
+        answer: "System file",
+    },
+    {
+        id: 3,
+        question: " What will a UPS be used for in a building?",
+        options: ["To provide power to essential equipment", "To carry messages between departments", "To control lighting and power systems" ],
+        answer: "To provide power to essential equipment"
+    },
+    {
+        id: 4,
+        question: "Who co-founded Hotmail in 1996 and then sold the company to Microsoft??",
+        options: ["Ada Byron Lovelace", "Sabeer Bhatia", "Ray Tomlinson"],
+        answer: "Sabeer Bhatia"
+    },
+    {
+        id: 5,
+        question: "Who built the world's first binary digit computer: Z1...??",
+        options: ["Konrad Zuse", "Alan Turing","George Boole"],
+        answer: "Konrad Zuse"
+    },
+]
+ 
+export default qBank;
